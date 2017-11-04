3627d082d57df1717633d765bbff7c13eb6c8b2e
hanekaoru <vd2o@foxmail.com>
2017-11-04 22:29:30 +0800

    Create ModelPages.js

---
+++ modules/ModelPages.js
@@ -0,0 +1,28 @@
+var Utils = require('./utils');
+
+function ModelPages(page, conf) {
+
+	this.query = this.qs = conf.query = conf.qs = Utils.Url.query;
+	this.page = page;
+	conf.pageName = this.page;
+	conf.originPageName = this.originPageName = location.pathname.replace(/.+\/(\w+)\.\w+$/, '$1').toLowerCase();
+	conf.http = this.http = function () {
+		var o = {};
+		['get', 'post'].forEach(m => {
+			o[m] = function () {
+				return $[m].apply(null, arguments);
+			}
+		});
+		return o;
+	}();
+	this.conf = conf;
+	Object.assign(this, conf);
+}
+
+ModelPages.prototype = {
+	dispatch: function (fn) {
+		$('body').attr('yinlian') && $('body').attr('yinlian').toLowerCase() == this.page.toLowerCase() && fn.call(this, this.conf);
+	}
+}
+
+module.exports = ModelPages;
